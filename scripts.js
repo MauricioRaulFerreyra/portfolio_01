819a89a3f89d2fdc6f2dadeba794d9dc3179ddc8
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-08-17 13:25:48 -0300

    a button is created in the about section1

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuButton = document.querySelector(".menu-button");
   const menu = document.querySelector(".menu");
   const menuItems = document.querySelectorAll(".menu a");
+  const contactButton = document.querySelector(".button-contact");
 
   menuButton.addEventListener("click", function () {
     menuButton.classList.toggle("open");
     menu.classList.toggle("open");
+    contactButton.classList.toggle("open");
   });
 
   menuItems.forEach(function (item) {
@@ -13,5 +15,9 @@ document.addEventListener("DOMContentLoaded", function () {
       menuButton.classList.remove("open");
       menu.classList.remove("open");
     });
+  });
+
+  contactButton.addEventListener("click", function () {
+    window.location.hash = "#contact";
   });
 });

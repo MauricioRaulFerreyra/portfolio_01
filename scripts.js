bed8c09656566a55d3591464dd690ac970017302
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-08-17 15:48:44 -0300

    the menu section was modified

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.querySelector(".menu-button");
-  const menu = document.querySelector(".menu");
-  const menuItems = document.querySelectorAll(".menu a");
-  const contactButton = document.querySelector(".button-contact");
+/*-------   Menu ------- */
 
-  menuButton.addEventListener("click", function () {
-    menuButton.classList.toggle("open");
-    menu.classList.toggle("open");
-    contactButton.classList.toggle("open");
+((d) => {
+  const $menuButton = d.querySelector(".menu-button"),
+    $menu = d.querySelector(".menu"),
+    $menuItems = d.querySelectorAll(".menu a"),
+    $contactButton = d.querySelector(".button-contact");
+
+  $menuButton.addEventListener("click", () => {
+    $menuButton.classList.toggle("open");
+    $menu.classList.toggle("open");
   });
 
-  menuItems.forEach(function (item) {
+  $menuItems.forEach(function (item) {
     item.addEventListener("click", function () {
-      menuButton.classList.remove("open");
-      menu.classList.remove("open");
+      $menuButton.classList.remove("open");
+      $menu.classList.remove("open");
     });
   });
 
-  contactButton.addEventListener("click", function () {
+  $contactButton.addEventListener("click", function () {
     window.location.hash = "#contact";
   });
-});
+})(document);
